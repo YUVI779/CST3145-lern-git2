e8689f384d316e4b6cdc7a954e023a76dab2533e
YUVI779 <Uvchauhan1@gmail.com>
2023-02-04 20:22:33 +0000

    created delete functionality

--- app.js
+++ app.js
@@ -41,7 +41,6 @@ async function MAINfunction() {
   }
 }
 
-
 routers.get("/lessons", (req, res, next) => {
   let SERVERREQUEST = MONGODBCONNECTION();
   AllDatabase(SERVERREQUEST).then((data) => {
@@ -51,7 +50,7 @@ routers.get("/lessons", (req, res, next) => {
 
 function MONGODBCONNECTION() {
   let SERVERREQUEST = new MongoClient(
-      `mongodb+srv://yuvi:yuvi123@cluster0.kh8erhi.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://yuvi:yuvi123@cluster0.kh8erhi.mongodb.net/?retryWrites=true&w=majority`
   );
   SERVERREQUEST.connect();
   return SERVERREQUEST;
@@ -68,18 +67,60 @@ routers.post("/lessons", (req, res, next) => {
     });
 });
 
-
-
 routers.get("/orders", (req, res, next) => {
-    let SERVERREQUEST = MONGODBCONNECTION();
-    getOrderList(SERVERREQUEST).then((data) => {
+  let SERVERREQUEST = MONGODBCONNECTION();
+  getOrderList(SERVERREQUEST).then((data) => {
     res.send(data);
+  });
 });
+
+async function DELETETHELessons(SERVERREQUEST, id) {
+  const result = await SERVERREQUEST.db("test")
+    .collection("lessonsItems")
+    .deleteOne({ _id: new ObjectId(id) }, (err, result) => {
+      SERVERREQUEST.close();
+    });
+  return result;
+}
+
+async function AllDatabase(product) {
+  const db = await product
+    .db("test")
+    .collection("lessonsItems")
+    .find()
+    .toArray();
+  if (db) {
+    return db;
+  } else {
+    const message = `NO CONTENT`;
+    return message;
+  }
+}
+
+routers.post("/orders", (req, res, nexttt) => {
+  let SERVERREQUEST = MONGODBCONNECTION();
+  OrderCreationMethod(SERVERREQUEST, req.body).then((RESPONSE) => {
+    if (RESPONSE) {
+      res.send(`Successfully Created`);
+    } else {
+      res.status(404).send(`${req.body.lessonName}  out of stock`);
+    }
+  });
 });
+
+async function getOrderList(product) {
+  const db = await product.db("test").collection("orders").find().toArray();
+  if (db) {
+    return db;
+  } else {
+    const message = `Sorry NO CONTENT available`;
+    return message;
+  }
+}
 
 routers.delete("/lessons/:id", (req, res) => {
-    let SERVERREQUEST = MONGODBCONNECTION();
-    DELETETHELessons(SERVERREQUEST, req.params.id)
+  let SERVERREQUEST = MONGODBCONNECTION();
+  DELETETHELessons(SERVERREQUEST, req.params.id)
     .then((RESPONSE) => {
       res.send(`deleted successfully`);
     })
@@ -88,71 +129,25 @@ routers.delete("/lessons/:id", (req, res) => {
     });
 });
 
-async function DELETETHELessons(SERVERREQUEST, id) {
-    const result = await SERVERREQUEST.db("test")
-    .collection("lessonsItems")
-    .deleteOne({ _id: new ObjectId(id) }, (err, result) => {
-        SERVERREQUEST.close();
-    });
-  return result;
-}
-
-
-async function AllDatabase(product) {
-    const db = await product
-      .db("test")
-      .collection("lessonsItems")
-      .find()
-      .toArray();
-    if (db) {
-      return db;
-    } else {
-      const message = `NO CONTENT`;
-      return message;
-    }
-}
-
-routers.post("/orders", (req, res, nexttt) => {
-  let SERVERREQUEST = MONGODBCONNECTION();
-  OrderCreationMethod(SERVERREQUEST, req.body)
-    .then((RESPONSE) => {
-        if (RESPONSE) {
-        res.send(`Successfully Created`);
-    } else {
-        res.status(404).send(`${req.body.lessonName}  out of stock`);
-      }
-    });
-});
-
-async function getOrderList(product) {
-  const db = await product.db("test").collection("orders").find().toArray();
-  if (db) {
-      return db;
-    } else {
-        const message = `Sorry NO CONTENT available`;
-    return message;
-}
-}
-
 routers.put("/lessons/:id", (req, res) => {
-    let SERVERREQUEST = MONGODBCONNECTION();
+  let SERVERREQUEST = MONGODBCONNECTION();
   LessonUpdate(SERVERREQUEST, req.params.id, req.body)
     .then((data) => {
       res.send(`Successfully updated the data`);
     })
     .catch((error) => {
-        res.status(404).send(error);
+      res.status(404).send(error);
     });
 });
 
 routers.delete("/orders", (req, res) => {
-    let SERVERREQUEST = MONGODBCONNECTION();
-    deleteOrders(SERVERREQUEST, req.params.id)
+  let SERVERREQUEST = MONGODBCONNECTION();
+  deleteOrders(SERVERREQUEST, req.params.id)
     .then((RESPONSE) => {
-        res.send(`deleted successfully`);
+      res.send(`deleted successfully`);
     })
     .catch((error) => {
-        res.status(404).send(error);
+      res.status(404).send(error);
     });
 });
 
@@ -160,14 +155,14 @@ async function SeacrhByName(SERVERREQUEST, searchedText) {
   let serachRESULT = await SERVERREQUEST.db("test")
     .collection("lessonsItems")
     .find({
-        name: searchedText,
+      name: searchedText,
     })
     .toArray();
-    return serachRESULT;
+  return serachRESULT;
 }
 
 async function deleteOrders(SERVERREQUEST, id) {
-    await SERVERREQUEST.db("test")
+  await SERVERREQUEST.db("test")
     .collection("orders")
     .deleteMany({})
     .then((res) => {})
@@ -176,42 +171,39 @@ async function deleteOrders(SERVERREQUEST, id) {
 }
 
 routers.post("/search", (req, res, nextss) => {
-    let SERVERREQUEST = MONGODBCONNECTION();
+  let SERVERREQUEST = MONGODBCONNECTION();
   SeacrhByName(SERVERREQUEST, req.body.text)
     .then((data) => {
-        res.send(data);
+      res.send(data);
     })
     .catch((error) => {
       res.status(404).send("ERRORSS");
     });
 });
 
-
 async function LessonUpdate(SERVERREQUEST, id, newData) {
-    const result = await SERVERREQUEST.db("test")
+  const result = await SERVERREQUEST.db("test")
     .collection("lessonsItems")
     .updateOne({ _id: new ObjectId(id) }, { $set: newData }, (err, result) => {
-        SERVERREQUEST.close();
+      SERVERREQUEST.close();
     });
-    return result;
+  return result;
 }
 
-
-
 async function CreateLESSONS(SERVERREQUEST, newListing) {
-    const result = await SERVERREQUEST.db("test")
+  const result = await SERVERREQUEST.db("test")
     .collection("lessonsItems")
     .insertOne(newListing);
-    return result;
+  return result;
 }
 
 async function OrderCreationMethod(server, SERVERREQUEST) {
-    let serverData = server.db("test").collection("orders");
-    let lessonSelected = await server
+  let serverData = server.db("test").collection("orders");
+  let lessonSelected = await server
     .db("test")
     .collection("lessonsItems")
     .findOne({
-        _id: new ObjectId(SERVERREQUEST.lessonId),
+      _id: new ObjectId(SERVERREQUEST.lessonId),
     });
   let id = lessonSelected._id.toString();
   if (lessonSelected.space) {
@@ -221,18 +213,18 @@ async function OrderCreationMethod(server, SERVERREQUEST) {
       .then((data) => {})
       .catch((error) => {
         console.log("error");
-    });
+      });
     return true;
   } else {
     return false;
-}
+  }
 }
 
 const StaticIMAGEMiddleware = (req, res) => {
-    const imagepath = path.join(__dirname, "image-lesson", req.url);
-    fs.stat(imagepath, (err, stats) => {
-        if (err) {
-            res.status(404).send("Empty folder");
+  const imagepath = path.join(__dirname, "image-lesson", req.url);
+  fs.stat(imagepath, (err, stats) => {
+    if (err) {
+      res.status(404).send("Empty folder");
       return;
     }
     fs.createReadStream(imagepath).pipe(res);
@@ -243,5 +235,5 @@ MAINfunction().catch(console.error);
 app.use(LOGGERmidleware);
 app.use("/", routers);
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
+  console.log(`http://localhost:${port}`);
 });
